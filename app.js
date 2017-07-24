42601a065aa63610e324e878f8c988ccf68f4803
Cole Palm <palm.cole@gmail.com>
2017-07-24 14:18:14 -0700

    prepare static build

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.use('/', index);
 app.use('/questions', questions);
